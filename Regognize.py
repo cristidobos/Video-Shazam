6ab9a528770b107a1cb6fa3f7a0606d3aaa1c225
Alex Grigore <42294736+AlexxGrigore@users.noreply.github.com>
2023-03-30 10:28:47 +0200

    Process video - still does not compile
    Added all the files from resources, created tools for reading audio and video. the process_video.py does not work yet. It seems to be the problem that it does not find the video

---
+++ Regognize.py
@@ -0,0 +1,66 @@
+import cv2
+import numpy as np
+import pyaudio
+import os
+
+import FileReader as file_reader
+
+
+def get_mp4_files(directory):
+    mp4_files = []
+    for filename in os.listdir(directory):
+        if filename.endswith('.mp4'):
+            name = os.path.splitext(filename)[0]
+            mp4_files.append(name)
+    return mp4_files
+
+
+def print_file_names(mp4_files):
+    print("Video library: \n")
+    index = 0
+    for file_name in mp4_files:
+        print("index " + str(index) + " -> " + file_name)
+        index += 1
+    print("\n----------------------\n")
+
+
+path_dataset = 'dataset/'
+path_training = 'training/recognition/'
+dataset_files = get_mp4_files(path_dataset)
+# print_file_names(dataset_files)
+
+"""
+Takes as an input the path to the cropped video, and returns the name of the best match
+
+input: path of the cropped video
+output: string with the name of the best match
+ 
+"""
+
+
+def recognize_video(video):
+    print(len(video.frames))
+
+
+def iterate_over_dataset(dataset_names, given_video, given_audio):
+    index = 0
+    for file_name in dataset_names:
+        video = file_reader.VideoReader(path_dataset + file_name + ".mp4")
+        # -------- play video
+        # video.display_video()
+
+        audio = file_reader.AudioReader(path_dataset + file_name + ".wav")
+        # -------- play audio
+        # audio.play_audio()
+        print("FINISHED reading DATASET sample")
+
+        index += 1
+        if index >= 1:
+            break
+
+
+train_video = file_reader.VideoReader(path_training + "video1" + ".mp4")
+train_audio = file_reader.AudioReader(path_training + "video1" + ".wav")
+print("FINISHED reading TRAINING sample")
+iterate_over_dataset(dataset_files, train_video, train_audio)
+
